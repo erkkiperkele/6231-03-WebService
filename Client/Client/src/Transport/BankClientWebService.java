93f5882773b507aae2437bba57c9a8c9ee80cc14
erkkiperkele <aymeric.grail@gmail.com>
2015-11-22 16:08:16 -0500

    Everything is implemented. Still Lots of dead code.

--- Client/Client/src/Transport/BankClientWebService.java
+++ Client/Client/src/Transport/BankClientWebService.java
@@ -1,54 +1,54 @@
-package Transport;
-
-import Contracts.ICustomerServer;
-import Contracts.IManagerServer;
-import Data.Bank;
-import Data.Customer;
-import Data.CustomerInfo;
-import Data.Loan;
-import Exceptions.RecordNotFoundException;
-import Exceptions.TransferException;
-
-import javax.security.auth.login.FailedLoginException;
-import java.util.Date;
-
-
-public class BankClientWebService implements ICustomerServer, IManagerServer {
-
-
-    @Override
-    public int openAccount(Bank bank, String firstName, String lastName, String emailAddress, String phoneNumber, String password) {
-
-        return 0;
-    }
-
-    @Override
-    public Customer getCustomer(Bank bank, String email, String password) throws FailedLoginException {
-        return null;
-    }
-
-    @Override
-    public Customer signIn(Bank bank, String email, String password) throws FailedLoginException {
-        return null;
-    }
-
-    @Override
-    public Loan getLoan(Bank bank, int accountNumber, String password, long loanAmount) throws FailedLoginException {
-        return null;
-    }
-
-    @Override
-    public Loan transferLoan(int loanId, Bank currentBank, Bank otherBank) throws TransferException {
-        return null;
-    }
-
-    @Override
-    public void delayPayment(Bank bank, int loanID, Date currentDueDate, Date newDueDate) throws RecordNotFoundException {
-
-    }
-
-    @Override
-    public CustomerInfo[] getCustomersInfo(Bank bank) throws FailedLoginException {
-        return new CustomerInfo[0];
-    }
-}
+//package Transport;
+//
+//import Contracts.ICustomerServer;
+//import Contracts.IManagerServer;
+//import Data.Bank;
+//import Data.Customer;
+//import Data.CustomerInfo;
+//import Data.Loan;
+//import Exceptions.RecordNotFoundException;
+//import Exceptions.TransferException;
+//
+//import javax.security.auth.login.FailedLoginException;
+//import java.util.Date;
+//
+//
+//public class BankClientWebService implements ICustomerServer, IManagerServer {
+//
+//
+//    @Override
+//    public int openAccount(Bank bank, String firstName, String lastName, String emailAddress, String phoneNumber, String password) {
+//
+//        return 0;
+//    }
+//
+//    @Override
+//    public Customer getCustomer(Bank bank, String email, String password) throws FailedLoginException {
+//        return null;
+//    }
+//
+//    @Override
+//    public Customer signIn(Bank bank, String email, String password) throws FailedLoginException {
+//        return null;
+//    }
+//
+//    @Override
+//    public Loan getLoan(Bank bank, int accountNumber, String password, long loanAmount) throws FailedLoginException {
+//        return null;
+//    }
+//
+//    @Override
+//    public Loan transferLoan(int loanId, Bank currentBank, Bank otherBank) throws TransferException {
+//        return null;
+//    }
+//
+//    @Override
+//    public void delayPayment(Bank bank, int loanID, Date currentDueDate, Date newDueDate) throws RecordNotFoundException {
+//
+//    }
+//
+//    @Override
+//    public CustomerInfo[] getCustomersInfo(Bank bank) throws FailedLoginException {
+//        return new CustomerInfo[0];
+//    }
+//}
--- Client/Client/src/WebServiceClient/BankWS.java
+++ Client/Client/src/WebServiceClient/BankWS.java
@@ -12,7 +12,11 @@ import WebServiceClient.FailedLoginException_Exception;
 
 //import javax.jws.WebService;
 import javax.security.auth.login.FailedLoginException;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 public class BankWS implements ICustomerServer, IManagerServer {
@@ -20,21 +24,8 @@ public class BankWS implements ICustomerServer, IManagerServer {
 
     private final BankWebService service;
 
-//    public static void main(String[] argv) {
-//        BankWebService service = new BankWebService_Service().getBankWebService();
-//
-//
-//        //invoke business method
-//        String test = service.sayHelloWorldFrom("Aymeric");
-//        System.out.println(test);
-//
-//    }
-
     public BankWS(Bank bank) {
         service = new BankWebService_Service().getBankWebService(bank);
-        //invoke business method
-        service.sayHelloWorldFrom("Aymeric");
-
     }
 
     @Override
@@ -79,8 +70,6 @@ public class BankWS implements ICustomerServer, IManagerServer {
     @Override
     public Loan transferLoan(int loanId, Bank currentBank, Bank otherBank) throws Exceptions.TransferException {
 
-//        return null;
-//
         try {
             return service.transferLoan(loanId, currentBank, otherBank);
         } catch (TransferException_Exception e) {
@@ -91,14 +80,27 @@ public class BankWS implements ICustomerServer, IManagerServer {
     @Override
     public void delayPayment(Bank bank, int loanID, Date currentDueDate, Date newDueDate) throws RecordNotFoundException {
         //TODO: Implement
-//        service.delayPayment(bank, loanID, currentDueDate, newDueDate);
+
+        GregorianCalendar c = new GregorianCalendar();
+
+
+        try {
+            c.setTime(currentDueDate);
+            XMLGregorianCalendar xmlCurrent = DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
+            c.setTime(newDueDate);
+            XMLGregorianCalendar xmlNew = DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
+
+            service.delayPayment(bank, loanID, xmlCurrent, xmlNew);
+        } catch (RecordNotFoundException_Exception e) {
+            throw new RecordNotFoundException(e.getMessage());
+        } catch (DatatypeConfigurationException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public CustomerInfo[] getCustomersInfo(Bank bank) throws FailedLoginException {
-//        return null;
 
-//        return new CustomerInfo[0];
         List<CustomerInfo> info;
         try {
             info = service.getCustomersInfo(bank);
